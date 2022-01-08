0bc33afb7a374f207e3b12601513375a841d67ca
Couleur <82747632+couleurm@users.noreply.github.com>
2022-01-08 20:44:17 +0100

    Initial push

---
+++ apps.ps1
@@ -0,0 +1,260 @@
+$apps = @{
+
+    'Tweaking, hardware, monitoring & benchmarking' = @{
+
+        CapFrameX = 'extras/CapFrameX'
+        Autoruns = 'extras/Autoruns'
+        DisplayDriverUninstaller = 'extras/ddu'
+        NSudo = 'extras/nsudo'
+        NVCleanstall = 'extras/nvcleanstall'
+        'CPU-Z' = 'extras/cpu-z'
+        'GPU-Z' = 'extras/gpu-z'
+        Prime95 = 'extras/prime95'
+        'MSI Kombustor' = 'extras/msikombustor'
+        'ProcessHacker2' = 'extras/processhacker'
+        'Ryzen controller (For AMD Ryzen laptops' = 'extras/ryzen-controller'
+        'Rivatuner Statistics Server (RTSS)' = 'extras/rtss'
+        ThrottleStop = 'extras/throttlestop'
+        'Winaero Tweaker' = 'extras/winaero-tweaker'
+        #procexp = 'extras/procexp'
+    }
+
+    'Game Launchers' = @{
+
+        Steam = 'versions/steam'
+        Uplay = 'versions/uplay'
+        'Ubisoft Connect' = 'versions/ubisoftconnect'
+        #'Heroic (Epic Games replacement)' = 'utils/heroic-games-launcher'
+    }
+
+    Disk_Stuff = @{
+
+        'Balena Etcher' = 'extras/etcher'
+        UNetbootin = 'extras/UNetbootin'
+        Rufus = 'extras/Rufus'
+        Ventoy = 'extras/Ventoy'
+        Bleachbit = 'extras/BleachBit'
+        CrystalDiskInfo = 'extras/CrystalDiskInfo'
+        CrystalDiskMark = 'extras/CrystalDiskMark'
+    }
+
+    Screenshotting = @{
+
+        ShareX = 'extras/ShareX'
+        Lightshot = 'extras/Lightshot'
+        Greenshot = 'extras/Greenshot'
+    }
+
+    'Image editors/viewers' = @{
+
+        'Paint.NET' = 'extras/paint.net'
+        Gimp = 'extras/Gimp'
+        Krita = 'extras/Krita'
+        Cupscale = 'extras/cupscale'
+        Irfanview = 'extras/irfanview'
+    }
+
+    'Media (video/audio)' = @{
+
+        #'AIO-Video' = 'utils/AIO-Video'
+        DiscordCompressor = 'utils/DiscordCompressor'
+        'OBS Studio' = 'extras/OBS-Studio'
+        LosslessCut = 'extras/LosslessCut'
+        Nmkoder = 'utils/nmkoder'
+        'MPV.net (user friendly mpv)' = 'extras/MPV.net'
+        MPV = 'extras/mpv'
+        VLC = 'extras/VLC'
+        FFmpeg = 'main/ffmpeg'
+        Audacity = 'extras/audacity'
+        Danser = 'utils/danser-go'
+        blur = 'utils/blur'
+        'MPC-HC' = 'extras/mpc-hc'
+        Staxrip = 'extras/staxrip'
+        Handbrake = 'extras/handbrake'
+        'Handbrake-CLI' = 'main/handbrake-cli'
+        gifski = 'main/gifksi'
+        gifsicle = 'main/gifsicle'
+        Shotcut = 'extras/shotcut'
+        Openshot = 'extras/openshot'
+        Blender = 'extras/blender'
+        VMAF = 'main/vmaf'
+        mkvtoolnix = 'extras/mkvtoolnix'
+        # OptiPNG = 'main/optipng'
+    }
+
+    Minecraft = @{
+
+        Minecraft = 'games/minecraft'
+        MultiMC = 'games/multimc'
+        'Temurin 17 JDK (adds itself to path as temurin17-javaw.exe)' = 'utils/temurin17-jdk'
+        Moony = 'utils/moony'
+    }
+
+    Social = @{
+
+        TeamSpeak3 = 'extras/teamspeak3'
+        Ferdi = 'extras/ferdi'
+        Discord = 'extras/discord'
+        Telegram = 'extras/Telegram'
+        'Discord Canary' = 'versions/discord-canary'
+        Ripcord = 'extras/ripcord'
+        Lightcord = 'extras/lightcord'
+        #Powercord = 'utils/powercord'
+        WhatsApp = 'extras/WhatsApp'
+        Skype = 'extras/skype'
+        Slack = 'extras/slack'
+        Signal = 'extras/Signal'
+        Joplin = 'extras/joplin'
+        Zoom = 'extras/zoom'
+    }
+
+    'Developping / Programming languages' = @{
+
+        'Windows Terminal' = 'extras/windows-terminal'
+        'Windows Terminal Quake (Dropdown for wt)' = 'extras/windows-terminal-quake'
+        'Notepad++' = 'extras/notepadplusplus'
+        'Sublime Text' = 'extras/sublime-text'
+        'VS Code' = 'extras/vscode'
+        'VS Codium' = 'extras/vscodium'
+        'Atom' = 'extras/atom'
+        'SciTe4AHK' = 'extras/scite'
+        Git = 'main/git'
+        'GitHub Desktop' = 'extras/github'
+        AutoHotkey = 'extras/autohotkey'
+        ElasticSearch = 'extras/ElasticSearch'
+        'Git LFS' = 'main/git-lfs'
+        Ruby = 'main/ruby'
+        NodeJS = 'main/NodeJS'
+        'NodeJS LTS' = 'main/NodeJS-LTS'
+        Python = 'main/python'
+        'pwsh (PowerShell 7)' = 'main/pwsh'
+        Golang = 'main/go'
+        'V-lang' = 'main/v'
+        Rust = 'main/rust'
+        Kotlin = 'main/kotlin'
+        Lua = 'main/lua'
+        'lua-for-windows' = 'main/lua-for-windows'
+        MongoDB = 'extras/mongodb'
+    
+    }
+
+    'CLI Tools' = @{
+
+        'Legendary (EGS CLI)' = 'games/legendary'
+        'yt-dlp' = 'main/yt-dlp'
+        'GitHub CLI (gh)' = 'main/gh'
+        'everything-cli' = 'main/everything-cli'
+        'Heroku CLI' = 'heroku-cli'
+        'Signal CLI' = 'main/signal-cli'
+        'SpeedTest(.net) CLI' = 'main/speedtest-cli'
+        OpenSSH = 'main/openssh'
+        'OpenSSL' = 'main/openssl'
+        curl = 'main/curl'
+        axel = 'main/axel'
+        wget = 'main/wget'
+        gcc = 'main/gcc'
+        Gitea = 'main/gitea'
+        grep = 'main/grep'
+        tar = 'main/tar'
+        x264 = 'main/x264'
+        x265 = 'main/x265'
+    }
+
+    'Aesthetics / quality of life' = @{
+
+        'Vibrance GUI' = 'extras/vibrance-gui'
+        PrivateZilla = 'extras/privatezilla'
+        ThisIsWin11 = 'extras/ThisIsWin11'
+        PowerToys = 'extras/powertoys'
+        EarTrumpet = 'extras/eartrumpet'
+        #Modern Flyouts = 'utils/modern-flyouts'
+        #Lively = 'utils/lively'
+        TaskbarX = 'extras/TaskbarX'
+        TranslucentTB = 'extras/TranslucentTB'
+        #RoundedTB = 'utils/RoundedTB'
+  
+    }
+
+    'Web browers / E-mail' = @{
+
+        Firefox = 'extras/firefox'
+        'Firefox Nightly' = 'versions/firefox-nightly'
+        Librewolf = 'extras/librewolf'
+        Waterfox = 'extras/waterfox'
+        Brave = 'extras/brave'
+        'Google Chrome' = 'extras/googlechrome'
+        'Ungoogled Chromium' = 'extras/ungoogled-chromium'
+        Vivaldi = 'extras/vivaldi'
+        Opera = 'extras/opera'
+        OperaGX = 'extras/opera-gx'
+        Thunderbird = 'extras/thunderbird-portable'
+        'Tor browser' = 'extras/tor-brower'
+    
+        #Icecat = 'utils/icecat'
+        #'Midori browser' = 'utils/midori-browser'
+    }
+
+    'Must haves' = @{
+
+       ' Everything 1.5ab (I rly recommend)' = 'extras/everything-alpha'
+        Everything = 'extras/everything'
+        '7-Zip' = 'main/7zip'
+        Syncthing = 'main/syncthing'
+    }
+
+    Peripherals = @{
+
+        OpenRGB = 'extras/openrgb'
+        'Logitech Gaming Software (non portable)' = 'nonportable/logitech-gaming-software-np'
+        #'Logitech Onboard Memory Manager' = 'utils/LogitechOMM'
+        #'Glorious Software' = 'utils/glorious-software'
+        #'LG Hub' = 'utils/lghub'
+        #'Razer Synapse 2' = 'utils/razer-synapse-2'
+        #'Razer Synapse 3' = 'utils/razer-synapse-3'
+        #'Roccat Swarm' = 'utils/roccat-swarm'
+    }
+
+    Writting = @{
+
+        Obsidian = 'extras/obsidian'
+        Notion = 'extras/notion'
+        Zettlr = 'extras/zettlr'
+        OpenOffice = 'extras/OpenOffice'
+        emacs = 'extras/emacs'
+        'OnlyOffice Desktop Editors' = 'extras/onlyoffice-desktopeditors'
+        'LibreOffice (Stable)' = 'extras/libre-office-stable'
+        'LaTeX (MikTeX)' = 'main/latex'
+        vim = 'main/vim'
+        neovim = 'main/neovim'
+        #'WPS Office Free' = 'utils/wps-office-free'
+    }
+    
+    Miscellaneous = @{
+        
+        'TeraCopy (non portable)' = 'nonportable/teracopy-np'
+        WinDirStat = 'extras/windirstat'
+        wiztree = 'extras/wiztree'
+        'Scoop Completion' = 'extras/scoop-completion'
+        Filezilla = 'extras/filezilla'
+        DeepL = 'extras/DeepL'
+        Onionshare = 'extras/onionshare'
+        Chatterino = 'extras/chatterino'
+        Bitwarden = 'extras/bitwarden'
+        KeepassXC = 'extras/keepassxc'
+        Keypirinha = 'extras/keypirinha'
+        Anydesk = 'extras/anydesk'
+        TeamViewer = 'extras/teamviewer'
+        Parsec = 'extras/parsec'
+        'WinRAR' = 'extras/winrar'
+        'Visual C++ redistributables' = 'utils/vcredist-aio'
+        '.NET SDK' = 'main/dotnet-sdk'
+        'DirectX Web' = 'utils/directx-web'
+        #'Cloudflare WARP' = 'utils/cloudflare-warp'
+        #'Sandboxie Plus' = 'utils/sandboxie-plus'
+        OpenVPN = 'extras/OpenVPN'
+        Virtualbox = 'extras/portable-virtualbox'
+        'Free Download Manager' = 'extras/freedownloadmanager'
+        QBitTorrent = 'extras/qbittorrent '
+        lessmsi = 'main/lessmsi'
+    }
+}
---
+++ installer.ps1
@@ -0,0 +1,143 @@
+$Protocols = [Enum]::GetNames([Net.SecurityProtocolType])
+Invoke-RestMethod https://raw.githubusercontent.com/couleurm/Write-Menu/master/Write-Menu.ps1 | Invoke-Expression
+
+if ($Protocols -contains 'Tls13'){[System.Net.ServicePointManager]::SecurityProtocol = 'Tls13'}
+elseif($Protocols -contains 'Tls12'){[System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'}
+else{
+    Write-Host "Could not find an adequate security protocol (TLS 1.2+), here's the list of all available protocols:" -ForegroundColor DarkRed
+    $Protocols
+    Pause
+    exit
+}
+
+<# -------------------------------------- Scoop check / install -------------------------------------- #>
+
+if (-Not(Get-Command scoop.cmd) -or (-Not($env:SCOOP))){
+
+    $Drives = Get-Volume | Where-Object {$PSItem.DriveLetter -notin '',$null} | ForEach-Object {"$($PSItem.DriveLetter):\ - $([Math]::Round($PSItem.SizeRemaining/1GB))GB Free"}
+    $Message = 'Scoop is not installed, on what drive would you like to install it?'
+    
+    $Selected = Write-Menu -Entries $Drives -Title $Message
+    $SelectedDrive = ($Selected[0..1] -join '')
+
+    if ($SelectedDrive -ne $env:HOMEDRIVE) {
+
+        $env:SCOOP =  Join-Path $SelectedDrive Scoop
+
+        if (Test-Path $env:SCOOP){
+            Write-Host "FATAL: A folder called scoop already exists at $env:SCOOP" -ForegroundColor Red -BackgroundColor Black
+            Pause
+            exit
+        }
+
+        [Environment]::SetEnvironmentVariable('SCOOP', $env:SCOOP, 'User')
+    
+    }
+
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+
+    Invoke-RestMethod https://get.scoop.sh | Invoke-Expression
+}
+
+<# -------------------------------------- Prompting user with choices -------------------------------------- #>
+
+if ($args -in '',$null,'-verbose'){
+
+    $script:ToInstall = @()
+
+    Write-Verbose "$PSScriptRoot" -Verbose
+
+    if ($PSScriptRoot){Invoke-Expression (Get-Content 'D:\GitHub\the-installer\apps.ps1' -Raw)}
+    else{Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/couleur-tweak-tips/the-installer/main/apps.ps1)}
+    
+
+    ForEach ($Section in $apps.Keys){
+
+        $Applications = ForEach($app in $apps.$Section){
+            $app.Keys
+        }
+        
+        $SelectedInSection = Write-Menu -Title ($Section -replace '_',' ') -Entries $Applications -MultiSelect
+
+        ForEach($SelectedApps in $SelectedInSection){
+            $ToInstall = $ToInstall + $apps.$Section.$SelectedApps
+        }
+    }
+
+}else{
+
+    switch ($args){
+        (($PSItem -is [string]) -and $PSItem -like "http*://*"){[array]$script:ToInstall = (Invoke-RestMethod $args) -Split ([SystemEnvironment]::NewLine)}
+        ($PSItem -is [array]){[array]$script:ToInstall = $args}
+    }
+
+}
+
+<# -------------------------------------- Adding buckets if not already done + essentials -------------------------------------- #>
+
+ForEach($app in ($essentials.keys)){
+
+    if (-Not(Get-Command $app -ErrorAction Ignore)){
+
+        if ($ToInstall.Contains($essentials.$app)){
+
+            $ToInstall = $ToInstall | Where-Object {$PSItem -ne $essentials.$app} # Removes them from the install list if they're in here
+        }
+        
+        Write-Warning "Installing $app (not a dependencie but strongly recommended)"
+        scoop.cmd install $app
+}
+}
+
+$Buckets = Join-Path (Get-Item (Get-Command scoop.cmd).Source).Directory.Parent.FullName buckets
+
+ForEach ($knownbucket in [array](scoop.cmd bucket known)){
+    if (($ToInstall -like "$knownbucket/*") -and (-Not(Test-Path (Join-Path $Buckets $knownbucket))) ){scoop.cmd bucket add $knownbucket}
+}
+if (($app -like "utils/*") -and (-Not(Test-Path (Join-Path $Buckets utils))) ){scoop.cmd bucket add utils 'https://github.com/couleur-tweak-tips/utils'}
+
+<# -------------------------------------- Installing the manifests --------------------------------------#>
+
+Write-Warning "Updating every scoop bucket.."
+scoop.cmd update
+
+$Scripts = $ToInstall -Like "https://*"
+
+if ($Scripts){
+
+    $ToInstall = $ToInstall | Where-Object {$PSItem -NotLike "https://*"}
+
+    ForEach ($Script in $Scripts){
+        Write-Warning "Installing script $Script"
+        Invoke-RestMethod $Script | Invoke-RestMethod
+    }
+
+}
+
+ForEach ($Package in $ToInstall){
+    $Count++
+    $Host.UI.RawUI.WindowTitle = "[$Count\$($ToInstall.Count)] - Installing $Package"
+    Write-Warning "Installing $Package"
+    scoop.cmd install $Package
+}
+<#
+$ToInstall = @(
+'extras/everything'
+'main/syncthing'
+'extras/everything-alpha'
+'extras/TaskbarX'
+'extras/BleachBit'
+'main/openssh'
+'extras/ddu'
+'main/NodeJS'
+'extras/Krita'
+'extras/discord-canary'
+'extras/emacs'
+'extras/openrgb'
+'extras/Lightshot'
+'games/Minecraft'
+'utils/DiscordCompressor'
+'extras/brave'
+'main/aria2'
+)
+#>
